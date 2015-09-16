9eebd1458f7bd98b44623059a4cdd628fb83b7ec
Kelvin Ly <kelvin.ly1618@gmail.com>
2015-09-15 22:01:23 -0400

    Sample code running

--- research/eeg.py
+++ research/eeg.py
@@ -1,2 +1,21 @@
 #!/usr/bin/env python2.7
 from emokit import emotiv
+import platform
+
+if platform.system() == "Windows":
+    import socket
+import gevent
+
+if __name__ == "__main__":
+  headset = emotiv.Emotiv()
+  gevent.spawn(headset.setup)
+  gevent.sleep(0)
+  try:
+    while True:
+      packet = headset.dequeue()
+      print packet.gyro_x, packet.gyro_y
+      gevent.sleep(0)
+  except KeyboardInterrupt:
+    headset.close()
+  finally:
+    headset.close()
